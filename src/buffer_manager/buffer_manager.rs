514154d9b3a264de23b7fc6536f4d385cdd51bde
yujikawa <ykyujikawa@gmail.com>
2025-03-02 05:59:16 +0900

    Modify buffer manager

--- src/buffer_manager/buffer_manager.rs
+++ src/buffer_manager/buffer_manager.rs
@@ -22,10 +22,12 @@ impl BufferFrame {
     }
 
     pub fn pin(&mut self) {
+        // 使用者が増えたらインクリメント
         self.pin_count += 1;
     }
 
     pub fn unpin(&mut self) {
+        // 使用者が減ればデクリメント
         if self.pin_count > 0 {
             self.pin_count -= 1;
         }
@@ -36,30 +38,36 @@ impl BufferFrame {
     }
 }
 
-pub struct BufferPool {
+pub struct BufferManager {
+    // Buffer poolを定義
     pool: Mutex<HashMap<BlockId, Arc<Mutex<BufferFrame>>>>,
     file_manager: Arc<FileManager>,
 }
 
-impl BufferPool {
+impl BufferManager {
     pub fn new(file_manager: Arc<FileManager>) -> Self {
-        BufferPool {
+        BufferManager {
             pool: Mutex::new(HashMap::new()),
             file_manager,
         }
     }
 
     pub fn pin_page(&self, block_id: &BlockId) -> Arc<Mutex<BufferFrame>> {
+        // Buffer poolの中身を探す
         let mut pool = self.pool.lock().unwrap();
 
+        // 目的のデータがある場合は使用中に設定してそのBuffer frameを返すだけ
         if let Some(frame) = pool.get(block_id) {
             frame.lock.unwrap().pin();
             return Arc::clone(frame);
         }
 
+        // 以下目的のデータがない場合
+        // ディスクから読み出しをして内容をページに保存
         let mut page = Page::new();
         self.file_manager.read(block_id, &mut page).unwrap();
 
+        // 取得したページをBuffer frame化、使用中、Frameをpoolに入れる
         let frame = Arc::new(Mutex::new(BufferFrame::new(page)));
         frame.lock().unwrap().pin();
         pool.insert(block_id.clone(), Arc::clone(&frame));
@@ -68,8 +76,10 @@ impl BufferPool {
     }
 
     pub fn unpin_page(&self, block_id: &BlockId, is_dirty: bool) {
+        // ピン留めを解除する
         let mut pool = self.pool.lock().unwrap();
 
+        // 対象があればピン留めを解除
         if let Some(frame) = pool.get(block_id) {
             let mut frame = frame.lock().unwrap();
             frame.unpin();
